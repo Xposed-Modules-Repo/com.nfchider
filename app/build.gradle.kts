b350938c87107a750654017cc81530df763d80b5
furina707 <furina707@users.noreply.github.com>
2026-09-22 16:11:10 +0800

    release: 4-1.1 - 升级版本号，支持真实定位获取与轨迹录制

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.nfchider"
         minSdk = 29
         targetSdk = 37
-        versionCode = 3
+        versionCode = 4
         versionName = "1.1"
     }
 
